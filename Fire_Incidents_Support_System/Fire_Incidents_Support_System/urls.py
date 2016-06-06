f9a37ea68321eeeb09b196df8818893567e9da98
user <user@LENOVO>
2016-06-06 18:06:28 +0900

    Updates!

--- Fire_Incidents_Support_System/Fire_Incidents_Support_System/urls.py
+++ Fire_Incidents_Support_System/Fire_Incidents_Support_System/urls.py
@@ -25,28 +25,28 @@ urlpatterns = [
     url(r'^normal', app.views.normal, name='normal'),
     url(r'^contact$', app.views.contact, name='contact'),
     url(r'^about', app.views.about, name='about'),
-    url(r'^login/$',
-        django.contrib.auth.views.login,
-        {
-            'template_name': 'app/login.html',
-            'authentication_form': app.forms.BootstrapAuthenticationForm,
-            'extra_context':
-            {
-                'title': 'Log in',
-                'year': datetime.now().year,
-            }
-        },
-        name='login'),
-    url(r'^logout$',
-        django.contrib.auth.views.logout,
-        {
-            'next_page': '/',
-        },
-        name='logout'),
+  ##  url(r'^login/$',
+  #  #    django.contrib.auth.views.login,
+  #      {
+  #          'template_name': 'app/login.html',
+  #          'authentication_form': app.forms.BootstrapAuthenticationForm,
+  #          'extra_context':
+  #          {
+  #              'title': 'Log in',
+  #              'year': datetime.now().year,
+  #          }
+  #      },
+  #      name='login'),
+   # url(r'^logout$',
+      #  django.contrib.auth.views.logout,
+     #   {
+      #      'next_page': '/',
+      #  },
+     #   name='logout'),
 
     # Uncomment the admin/doc line below to enable admin documentation:
-    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    #url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    url(r'^admin/', include(admin.site.urls)),
+    #url(r'^admin/', include(admin.site.urls)),
 ]
